1fb8c572a0d29028efa4b4fe81186ce5df4da0a3
Ruby <darkdragongaming379@gmail.com>
2025-08-20 16:39:45 +0100

    update to new smods apis

--- NoMoreEs.lua
+++ NoMoreEs.lua
@@ -24,14 +24,6 @@ function get_blind_amount(ante)
 	return ret
 end
 
-if Entropy then
-	local chipmultref = Entropy.get_chipmult_score
-	function Entropy.get_chipmult_score(...)
-		local ret = chipmultref(...)
-		if to_big(ret) > to_big(1e10) then return 10^400 end
-		return ret
-	end	
-end
 
 function number_format(num)
 	G.E_SWITCH_POINT = G.E_SWITCH_POINT or 100000000000
@@ -45,10 +37,9 @@ function number_format(num)
 	return string.format(num ~= math.floor(num) and (num >= 100 and "%.0f" or num >= 10 and "%.1f" or "%.2f") or "%.0f", num):reverse():gsub("(%d%d%d)", "%1,"):gsub(",$", ""):reverse()
 end
 
-NES = {
-	get_chipmult_score = function(mult, chips)
-		local ret = mult * chips
-		if to_big(ret) > to_big(1e10) then return 10^400 end
-		return ret
-	end
-}
+local ref = SMODS.calculate_round_score
+function SMODS.calculate_round_score(...)
+	local score = ref(...)
+	if to_big(score) > to_big(1e10) then return 10^400 end
+	return score
+end
